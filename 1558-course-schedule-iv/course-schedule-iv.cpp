81fbab743e7ecc37f50e87886b4fa5185aeb9a74
Tushar Tanish <133215507+tushartanish10@users.noreply.github.com>
2025-01-27 22:50:43 +0530

    Time: 13 ms (90.32%) | Memory: 65.3 MB (83.60%) - LeetSync

---
+++ 1558-course-schedule-iv/course-schedule-iv.cpp
@@ -0,0 +1,34 @@
+class Solution {
+ public:
+  vector<bool> checkIfPrerequisite(int numCourses,
+    vector<vector<int>>& prerequisites,vector<vector<int>>& queries) {
+    vector<bool> ans;
+    vector<vector<int>> graph(numCourses);
+    vector<vector<bool>> isPrerequisite(numCourses, vector<bool>(numCourses));
+
+    for (const vector<int>& prerequisite : prerequisites) {
+      const int u = prerequisite[0];
+      const int v = prerequisite[1];
+      graph[u].push_back(v);
+    }
+    for (int i = 0; i < numCourses; ++i)
+      dfs(graph, i, isPrerequisite[i]);
+
+    for (const vector<int>& query : queries) {
+      const int u = query[0];
+      const int v = query[1];
+      ans.push_back(isPrerequisite[u][v]);
+    }
+
+    return ans;
+  }
+
+  void dfs(const vector<vector<int>>& graph, int u, vector<bool>& used) {
+    for (const int v : graph[u]) {
+      if (used[v])
+        continue;
+      used[v] = true;
+      dfs(graph, v, used);
+    }
+  }
+};
